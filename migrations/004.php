9f310fae50fcfc48a5b14d85a85268cee59528fa
Lukas Fürderer <l.fuerderer@gmail.com>
2021-01-05 10:23:06 +0100

    Add a column for the creation date of ssh keys

---
+++ migrations/004.php
@@ -0,0 +1,7 @@
+<?php
+$migration_name = 'Add a column for the creation date of ssh keys';
+
+$this->database->query("
+ALTER TABLE `public_key`
+ADD `creation_date` date NULL DEFAULT CURDATE() AFTER `entity_id`
+");
