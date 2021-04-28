6dc9d0416d63cb40a94eab23a8533f11e0555c9a
Lukas Fürderer <l.fuerderer@gmail.com>
2021-04-28 10:12:29 +0200

    Add the key_scan column to server by migration
    This is the setting for the scan mode

--- model/migrationdirectory.php
+++ model/migrationdirectory.php
@@ -22,7 +22,7 @@ class MigrationDirectory extends DBDirectory {
 	/**
 	* Increment this constant to activate a new migration from the migrations directory
 	*/
-	const LAST_MIGRATION = 8;
+	const LAST_MIGRATION = 9;
 
 	public function __construct() {
 		parent::__construct();
